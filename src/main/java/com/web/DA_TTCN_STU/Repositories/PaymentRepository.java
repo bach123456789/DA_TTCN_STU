54ae377ee450c66388e42bad3ad7100948f31e0d
bach123456789 <bachnguyen2080@gmail.com>
2025-11-28 16:34:33 +0700

    xóa PaymentRepository.java

--- src/main/java/com/web/DA_TTCN_STU/Repositories/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.web.DA_TTCN_STU.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
