4e8e4e791ff52c384553237b49855e661ffb09af
Learncode2004 <lukyhoc789@gmail.com>
2026-01-04 14:06:09 +0700

    Fix admin-index.html : DONE

--- src/main/java/com/web/DA_TTCN_STU/DaTtcnStuApplication.java
+++ src/main/java/com/web/DA_TTCN_STU/DaTtcnStuApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DaTtcnStuApplication {
+public class    DaTtcnStuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaTtcnStuApplication.class, args);
