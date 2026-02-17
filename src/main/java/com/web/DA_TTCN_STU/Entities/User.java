820bd54882e0865e952c85f297bf13155accba63
bach123456789 <bachnguyen2080@gmail.com>
2026-02-17 23:41:30 +0700

    - Thêm annotation @Table(name = "users") để không bị trùng từ khóa "users" trong PostgreSQL

--- src/main/java/com/web/DA_TTCN_STU/Entities/User.java
+++ src/main/java/com/web/DA_TTCN_STU/Entities/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
